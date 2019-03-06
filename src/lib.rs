96f7d66639100a4ba6ff4830d8117d0ea21b91f4
Stein Somers <git@steinsomers.be>
2019-03-06 11:13:09 +0100

    benchmarks saying that un-inlining boosts performance

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#![feature(test)]
+extern crate test;
+
+use std::collections::BTreeSet;
+use test::{black_box, Bencher};
+
+const N: u32 = 1;
+
+fn btreeset_peek<T: Clone>(set: &BTreeSet<T>) -> T {
+    set.iter().next().unwrap().clone()
+}
+
+#[bench]
+pub fn btreeset_peek_fast(b: &mut Bencher) {
+    let s: BTreeSet<_> = (0..N).collect();
+    b.iter(|| {
+        let set = black_box(&s);
+        let elt = btreeset_peek(set);
+        black_box(elt);
+    })
+}
+
+#[bench]
+pub fn btreeset_peek_slow(b: &mut Bencher) {
+    let s: BTreeSet<_> = (0..N).collect();
+    b.iter(|| {
+        let set = black_box(&s);
+        let elt = set.iter().next().unwrap().clone();
+        black_box(elt);
+    })
+}
